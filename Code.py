5ed40f7a927194518ea48287e429bd0d70ef48be
DanielJennings2001 <danjenno11@gmail.com>
2017-04-07 12:18:19 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -22,23 +22,23 @@ def menu():
 		
 		
 		
-	elif choice == 3:
+	elif choice == 4:
 		
 		
 		
-	elif choice == 3:
+	elif choice == 5:
 		
 		
 		
-	elif choice == 3:
+	elif choice == 6:
 		
 		
 		
-	elif choice == 3:
+	elif choice == 7:
 		
 		
 		
-	elif choice == 3:
+	elif choice == 8:
 		
 		
 		
